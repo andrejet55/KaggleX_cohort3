be6f28d81c3ed5161e3434d8ef42d22f6931235b
Andrea Franco <andrefrak@hotmail.com>
2023-10-20 12:30:48 -0500

    added new csv path x3

--- datasets/zni_data.py
+++ datasets/zni_data.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-zni_df = pd.read_csv('datasets\zni_data.py')
+zni_df = pd.read_csv('datasets/zni_data.py')
 
 zni_df = zni_df.rename(columns={'ID DEPATAMENTO':'PROVINCE_ID', 'DEPARTAMENTO':'PROVINCE','ID MUNICIPIO':'CITY_ID','MUNICIPIO':'CITY','ID LOCALIDAD':'ZONE_ID','LOCALIDAD':'ZONE','AÑO SERVICIO':'SERVICE_YEAR', 
                                 'MES SERVICIO':'SERVICE_MONTH','ENERGÍA ACTIVA':'ACTIVE_POWER','ENERGÍA REACTIVA':'REACTIVE_POWER','POTENCIA MÁXIMA':'MAX_POWER',
