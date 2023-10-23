c6f5239d6bc9a0652d05ccaf631f8a21e62e8fef
Andrea Franco <andrefrak@hotmail.com>
2023-10-23 01:02:06 -0500

    first version of the dahsboard with plotly

--- dash_app.py
+++ dash_app.py
@@ -2,6 +2,7 @@
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
+import visualize
 
 # Incorporate data
 zni_df = pd.read_csv('datasets/Energia_en_ZNI.csv')
@@ -12,62 +13,135 @@ zni_df = zni_df.rename(columns={'ID DEPATAMENTO':'PROVINCE_ID', 'DEPARTAMENTO':'
     
 
 # Initialize the app - incorporate css
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/litera/bootstrap.min.css']
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-# App layout
 app.layout = html.Div([
-    html.Div(className='row', children='Dashboard of the Non-Interconnected Areas in Colombia',
-             style={'textAlign': 'center', 'color': 'blue', 'fontSize': 30}),
+    html.H1("Dashboard of the Non-Interconnected Areas in Colombia", className="title",style={'color':'#008080','textAlign': 'center'}),  # Set the title
+    # Add a blank space using CSS style
+    html.Div(style={'margin-top': '20px'}),
+    html.H4("Energy consumption analysis for the non-interconnected areas of Colombia", className="alert-heading", style={'textAlign': 'center'}),
+                html.A("The non-interconnected zones (ZNI) are regions that do not receive public electricity service through the national grid, also known as the national interconnected system (SIN)", className="alert-link", href="#", style={'textAlign': 'center'}),
+    html.Div(children = '''
+            Dataset: 
+    ''',style={'margin-left': '20px', 'margin-right': '20px','textAlign': 'center'}),
+
+    html.Div(style={'margin-top': '20px'}),
 
     html.Div(className='row', children=[
-        dcc.RadioItems(options=['ZONE', 'CITY', 'PROVINCE'],
-                       value='ZONE',
-                       inline=True,
-                       id='my-radio-buttons-final')
+        html.Div(style={'margin-left': '100px', 'margin-right': '20px','width': '85%', 'height': 'auto'},className='six columns', children=[
+            dash_table.DataTable(data=zni_df.to_dict('records'), page_size=11, style_table={'overflowX': 'auto'})
+        ])
     ]),
 
-    html.Div(className='row', children=[
+    html.H2("Active power peak value by location", className="subtitle",style={'color':'#008080','textAlign': 'center'}),
+    html.Div(style={'margin-top': '20px'}),
+    html.Div(children = '''
+            Choose a type of location to display:
+    ''',style={'margin-left': '20px', 'margin-right': '20px','textAlign': 'center'}),
+    html.Div(style={'margin-top': '20px'}),
+    # Create a new row for the RadioItems
+    html.Div(style={'margin-left': '20px'},className='row', children=[
         html.Div(className='six columns', children=[
-            dash_table.DataTable(data=zni_df.to_dict('records'), page_size=11, style_table={'overflowX': 'auto'})
-        ]),
+            dcc.RadioItems(
+                options=['ZONE', 'CITY', 'PROVINCE'],
+                value='ZONE',
+                inline=True,
+                id='my-radio-buttons-final',
+                labelStyle={'display': 'block', 'margin-bottom': '10px'}
+                
+            )
+        ])
+    ]),
+    
+    # Create a new row for the last two graphs (placed in the same row)
+    html.Div(style={'margin-left': '20px', 'margin-right': '20px'},className='row', children=[
         html.Div(className='six columns', children=[
             dcc.Graph(figure={}, id='bar-chart-high-act-en')
-
         ]),
-        html.Div(className='six columns', children=[
+        html.Div(className='six columns2', children=[
             dcc.Graph(figure={}, id='bar-chart-low-act-en')
-
         ])
-    ])
+    ]),
+
+    
+    html.H2("Active energy analysis", className="subtitle",style={'color':'#008080','textAlign': 'center'}),
+    html.Div(style={'margin-top': '20px'}),
+    html.H3("Mean active energy", className="subtitle",style={'textAlign': 'center'}),
+    # Create a new row for the last two graphs (placed in the same row)
+    
+    html.Div(className='row', children=[
+        html.Div(className='twelve columns', children=[
+            
+            dcc.Graph(figure={}, id='mean-act-e')  # Display the Plotly figure in this new row
+        ]),
+        html.Div(className='twelve columns', children=[
+            
+            dcc.Graph(figure={}, id='mean-act-p-prov')  # Display the Plotly figure in this new row
+        ])
+    ]),
+
+    html.Div(style={'margin-top': '20px'}),
+    html.H3("Hours of power demanded", className="subtitle",style={'textAlign': 'center'}),
+    html.Div(style={'margin-top': '20px'}),
+    html.Div(className='row', children=[
+        html.Div(className='twenty-four columns', children=[
+            
+            dcc.Graph(figure={}, id='mean-h')  # Display the Plotly figure in this new row
+        ]),
+        html.Div(className='twenty-four columns', children=[
+            
+            dcc.Graph(figure={}, id='mean-h-z')  # Display the Plotly figure in this new row
+        ])
+    ]),
+
+    html.Div(style={'margin-top': '20px'}),
+    html.H3("Power Factor", className="subtitle",style={'textAlign': 'center'}),
+    html.Div(style={'margin-top': '20px'}),
+    html.Div(className='row', children=[
+        html.Div(style={'display': 'flex', 'justify-content': 'center'},className='twenty-four columns', children=[
+            
+            dcc.Graph(figure={}, id='h_power-factor')  # Display the Plotly figure in this new row
+        ]),
+        html.Div(style={'display': 'flex', 'justify-content': 'center'},className='twenty-four columns', children=[
+            
+            dcc.Graph(figure={}, id='l_power-factor')  # Display the Plotly figure in this new row
+        ])
+    ]),
+    
+    html.Div(style={'margin-top': '20px'}),
+    html.H2("Wind Power", className="subtitle",style={'textAlign': 'center'}),
+    html.Img(src="assets\powerdensityimg.jpg", style={'width': '95%', 'height': 'auto','textAlign': 'center'}),
+    
+
+    html.Div(style={'margin-top': '20px'}),
+    html.H2("Solar Atlas", className="subtitle",style={'width': '95%', 'height': 'auto','textAlign': 'center'}),
+    html.Div(style={'margin-top': '20px'}),
+    html.Div(className='row', children=[
+        html.Div(style={'margin-left': '400px', 'margin-right': '20px'},className='twenty-four columns', children=[
+            
+            dcc.Graph(figure={}, id='solar_atlas')  # Display the Plotly figure in this new row
+        ]),
+        #html.Div(className='twenty-four columns', children=[
+            
+        #    dcc.Graph(figure={}, id='solar_atlas2')  # Display the Plotly figure in this new row
+        #]),
+        
+    ]),
 ])
+
+
 
 # Add controls to build the interaction
 @callback(
     Output(component_id='bar-chart-high-act-en', component_property='figure'),
     Input(component_id='my-radio-buttons-final', component_property='value')
 )
-def update_graph(location):
-    
-    max_power = zni_df.groupby(location).MAX_POWER.max().sort_values(ascending=False)
-    max_power = pd.DataFrame(max_power).reset_index()
-
-    # Remove parentheses and their contents for visualization purposes
-    max_power[location] = max_power[location].str.replace(r'\s*\(.*\)', '', regex=True)
-
-    #print(max_power)
-
-    # Create bar chart
-    fig1=px.bar(max_power[:5],x=location,y="MAX_POWER",title="Highest 'Maximum power' service daily sample")
-    # Update marker color
-    fig1.update_traces(marker=dict(color='#008080'))
-
-    # Set automargin for x-axis tick labels to True
-    fig1.update_xaxes(automargin=True)
-
-    return fig1
+def update_high_bar_chart(location):
+    fig = visualize.max_power(location)
+    return fig
 
 
 @callback(
@@ -75,27 +149,81 @@ def update_graph(location):
     Output(component_id='bar-chart-low-act-en', component_property='figure'),
     Input(component_id='my-radio-buttons-final', component_property='value')
 )
-def update_graph(location):
-    
-    max_power = zni_df.groupby(location).MAX_POWER.max().sort_values(ascending=False)
-    max_power = pd.DataFrame(max_power).reset_index()
-
-    # Remove parentheses and their contents for visualization purposes
-    max_power[location] = max_power[location].str.replace(r'\s*\(.*\)', '', regex=True)
-
-    #print(max_power)
-
-    # Create bar chart
-    fig2=px.bar(max_power[-5:],x=location,y="MAX_POWER",title="Lowest 'Maximum power' service daily sample")
-    # Update marker color
-    fig2.update_traces(marker=dict(color='#008080'))
-
-    # Set automargin for x-axis tick labels to True
-    fig2.update_xaxes(automargin=True)
-
+def update_low_bar_chart(location):
+    fig2 = visualize.max_power(location, range='min')
     return fig2
 
+@callback(
 
+    Output(component_id='mean-act-e', component_property='figure'),
+    Input(component_id='my-radio-buttons-final', component_property='value')
+)
+def mean_act_chart(location):
+    fig2 = visualize.mean_act_energy_year()
+    return fig2
+
+@callback(
+
+    Output(component_id='mean-act-p-prov', component_property='figure'),
+    Input(component_id='my-radio-buttons-final', component_property='value')
+)
+def mean_zone_act_chart(location):
+    fig2 = visualize.mean_act_power_province()
+    return fig2
+
+@callback(
+
+    Output(component_id='mean-h', component_property='figure'),
+    Input(component_id='my-radio-buttons-final', component_property='value')
+)
+def mean_h(location):
+    fig2 = visualize.mean_hours()
+    return fig2
+
+@callback(
+
+    Output(component_id='mean-h-z', component_property='figure'),
+    Input(component_id='my-radio-buttons-final', component_property='value')
+)
+def mean_h_z(location):
+    fig2 = visualize.mean_hours_zones()
+    return fig2
+
+@callback(
+
+    Output(component_id='l_power-factor', component_property='figure'),
+    Input(component_id='my-radio-buttons-final', component_property='value')
+)
+def h_power_factor(location):
+    fig2 = visualize.power_factor(location)
+    return fig2
+@callback(
+
+    Output(component_id='h_power-factor', component_property='figure'),
+    Input(component_id='my-radio-buttons-final', component_property='value')
+)
+def l_power_factor(location):
+    fig2 = visualize.power_factor(location,range='min')
+    return fig2
+
+@callback(
+
+    Output(component_id='solar_atlas', component_property='figure'),
+    Input(component_id='my-radio-buttons-final', component_property='value')
+)
+def solar_atlas(location):
+    fig2 = visualize.solar_map()
+    return fig2
+
+'''@callback(
+
+    Output(component_id='solar_atlas2', component_property='figure'),
+    Input(component_id='my-radio-buttons-final', component_property='value')
+)
+def solar_atlas(location):
+    fig2 = visualize.solar_map2()
+    return fig2
+'''
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
--- visualize.py
+++ visualize.py
@@ -1,20 +1,20 @@
 import numpy as np
 import pandas as pd
+import math
 import matplotlib.pyplot as plt
 import seaborn as sns
+import geopandas as gpd
 import plotly.express as px
 import plotly.graph_objects as go 
-from plotly.subplots import make_subplots
 import plotly.figure_factory as ff # for plot heatmap
 from plotly.offline import init_notebook_mode
 init_notebook_mode(connected=True)
 import warnings
 warnings.filterwarnings('ignore')
 
-# helpful modules
-import fuzzywuzzy
-from fuzzywuzzy import process
-import charset_normalizer
+import rasterio as rio
+
+from matplotlib.colors import LightSource
 
 # Tips data available in the plotly express module
 df_tips=px.data.tips()
@@ -33,34 +33,252 @@ zni_df = zni_df.rename(columns={'ID DEPATAMENTO':'PROVINCE_ID', 'DEPARTAMENTO':'
 
 
 #Show first 5 zones with lowest and highest 'max power' values
-def show_max_power(location):
+def max_power(location, range='max'):
     max_power = zni_df.groupby(location).MAX_POWER.max().sort_values(ascending=False)
     max_power = pd.DataFrame(max_power).reset_index()
 
     # Remove parentheses and their contents for visualization purposes
     max_power[location] = max_power[location].str.replace(r'\s*\(.*\)', '', regex=True)
 
-    #print(max_power)
-
-    # Create bar chart
-    fig1=px.bar(max_power[:5],x=location,y="MAX_POWER",title="Highest 'Maximum power' service daily sample")
-    # Update marker color
-    fig1.update_traces(marker=dict(color='#008080'))
-
-    # Set automargin for x-axis tick labels to True
-    fig1.update_xaxes(automargin=True)
-
-    # Show the plot
-    fig1.show()
-
+    if range == 'max':
+        # Create bar chart
+        fig=px.bar(max_power[:5],x=location,y="MAX_POWER",title="Highest 'Maximum power' service daily sample")
+    else: 
+        # Create bar chart
+        fig=px.bar(max_power[-5:],x=location,y="MAX_POWER",title="Lowest 'Maximum power' service daily sample")
     
-
-    # Create bar chart
-    fig=px.bar(max_power[-5:],x=location,y="MAX_POWER",title="Lowest 'Maximum power' service daily sample")
     # Update marker color
     fig.update_traces(marker=dict(color='#008080'))
 
     # Set automargin for x-axis tick labels to True
     fig.update_xaxes(automargin=True)
 
- 
+    return fig
+    
+def mean_act_energy_year():
+    
+    years = [2020, 2021,2022,2023]
+    result = pd.DataFrame(columns=years)
+    
+    for year in years:
+        power = zni_df.loc[zni_df['SERVICE_YEAR'] == year]
+        power = power.groupby('SERVICE_MONTH').ACTIVE_POWER.mean()
+        result[year] = power
+    
+    total_act_energy = zni_df.groupby('SERVICE_MONTH').ACTIVE_POWER.mean()
+    result['TOTAL'] = total_act_energy
+    
+    mean_act_energy_year = result
+    mean_act_energy_year = mean_act_energy_year.reset_index()
+
+    # Create a figure object for which we can later add the plots
+    fig = go.Figure()
+
+    # pass the graph objects to the add trace method, assign a series to x and y parameters of graph objects.
+    fig.add_trace(go.Scatter(x=mean_act_energy_year['SERVICE_MONTH'], y=mean_act_energy_year[2020], mode='lines',line_color='#AF4343' ,name='2020'))
+    fig.add_trace(go.Scatter(x=mean_act_energy_year['SERVICE_MONTH'], y=mean_act_energy_year[2021], mode='lines+markers',line_color='#fcbca2' ,name='2021'))
+
+    # Customizing a particular line
+    fig.add_trace(go.Scatter(x=mean_act_energy_year['SERVICE_MONTH'], y=mean_act_energy_year[2022], 
+                            mode='lines+markers', name='2022',line_color='red'  ,                   
+                            line=dict(color='darkgreen', dash='dot')))
+
+    fig.add_trace(go.Scatter(x=mean_act_energy_year['SERVICE_MONTH'], y=mean_act_energy_year[2023], mode='lines+markers',line_color='#6A5ACD' ,name='2023'))
+    # Further style the figure
+
+    temp = dict(layout=go.Layout(font=dict(family="Franklin Gothic", size=12)))
+    fig.update_traces(marker=dict(line=dict(width=1, color='#000000')))
+    fig.update_layout(title="Mean demand per month (All zones)", showlegend=True, template=temp, 
+                    legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="right", x=.97),
+                    barmode='group', bargap=.15)
+
+    return fig
+
+def loc_m_act_energy_year(location):
+
+    years = [2020, 2021,2022,2023]
+    result = pd.DataFrame(columns=years)
+
+    for year in years:
+        hours = zni_df.loc[zni_df['SERVICE_YEAR'] == year]
+        total_hours = hours.groupby(location).ACTIVE_POWER.mean()
+        result[year] = total_hours
+
+    total_hours = zni_df.groupby(location).ACTIVE_POWER.mean()
+    result['TOTAL'] = total_hours
+    result = result.reset_index()
+    
+    return result
+
+
+
+def mean_act_power_province():
+    pal = sns.color_palette("mako", 5).as_hex()
+    province_m_act_energy = loc_m_act_energy_year('PROVINCE')
+    province_m_act_energy = province_m_act_energy.reset_index()
+
+    fig = px.pie(province_m_act_energy, values='TOTAL', names='PROVINCE', title='Mean active power by province',color_discrete_sequence=pal)
+
+    #fig.update_traces(hoverinfo='label+percent', textfont_size=15, 
+    #                  textinfo="label+percent ", pull=[0.05, 0, 0, 0, 0],
+    #                  marker_line=dict(color="#FFFFFF", width=2))
+
+    temp = dict(layout=go.Layout(font=dict(family="Franklin Gothic", size=12)))
+    fig.update_layout(template=temp, title='Mean active power by province', 
+                    uniformtext_minsize=15, uniformtext_mode='hide',width=800)
+    return fig
+
+def sum_h_year(location):
+    
+    years = [2020, 2021,2022,2023]
+    result = pd.DataFrame(columns=years)
+    
+    for year in years:
+        hours = zni_df.loc[zni_df['SERVICE_YEAR'] == year]
+        total_hours = hours.groupby(location).DAILY_MEAN_HOURS.mean()
+        result[year] = total_hours
+    
+    total_hours = zni_df.groupby(location).DAILY_MEAN_HOURS.mean()
+    result['TOTAL'] = total_hours
+    return result
+
+def mean_hours():
+
+    province_hours = sum_h_year('PROVINCE')
+    pal = sns.color_palette("mako", 5).as_hex()
+    province_hours = province_hours.reset_index()
+
+    fig = px.pie(province_hours, values='TOTAL', names='PROVINCE', title='Mean service hours by province',color_discrete_sequence=pal)
+
+    #fig.update_traces(hoverinfo='label+percent', textfont_size=15, 
+    #                  textinfo="label+percent ", pull=[0.05, 0, 0, 0, 0],
+    #                  marker_line=dict(color="#FFFFFF", width=2))
+
+    temp = dict(layout=go.Layout(font=dict(family="Franklin Gothic", size=12)))
+    fig.update_layout(template=temp, title='Mean service hours by province', 
+                    uniformtext_minsize=15, uniformtext_mode='hide',width=800)
+    return fig
+
+def mean_hours_zones():
+    names_24to18,names_17to12,names_11to6,names_5to0 = [], [], [], []
+    n_24to18,n_17to12,n_11to6,n_5to0= 0,0,0,0
+
+    zone_hours = sum_h_year('ZONE')
+
+    for i in range(0,102):
+        if zone_hours['TOTAL'].iloc[i] >17:
+            
+            n_24to18 += 1
+            names_24to18.append(zone_hours['TOTAL'].index[i])
+
+        elif zone_hours['TOTAL'].iloc[i] <= 17 and zone_hours['TOTAL'].iloc[i] >=12:
+
+            n_17to12 += 1
+            names_17to12.append(zone_hours['TOTAL'].index[i])
+            
+        elif zone_hours['TOTAL'].iloc[i] <= 11 and zone_hours['TOTAL'].iloc[i] >=6:
+
+            n_11to6 += 1
+            names_11to6.append(zone_hours['TOTAL'].index[i])
+        
+        else:
+
+            n_5to0 += 1
+            names_5to0.append(zone_hours['TOTAL'].index[i])
+
+    pal = sns.color_palette("mako", 5).as_hex()
+
+    dic = {'Number of hours':['24 to 18','17 to 12', '11 to 6','5 to 0'],
+        'Value':[n_24to18,n_17to12,n_11to6,n_5to0]}
+
+    n_hours_per_zone = pd.DataFrame(dic)
+
+    fig = px.pie(n_hours_per_zone, values='Value', names='Number of hours', title='Zones divided by amount of mean daily hours of power consumption',color_discrete_sequence=pal)
+
+    fig.update_traces(hoverinfo='label+percent', textfont_size=15, 
+                    textinfo="label+percent ", pull=[0.05, 0, 0, 0, 0],
+                    marker_line=dict(color="#FFFFFF", width=2))
+
+    temp = dict(layout=go.Layout(font=dict(family="Franklin Gothic", size=12)))
+    fig.update_layout(template=temp, title='Hours of power consumption', 
+                    uniformtext_minsize=15, uniformtext_mode='hide',width=800)
+    
+    return fig
+
+# Calculate the Power Factor for each row
+def calculate_power_factor(row):
+    if row['ACTIVE_POWER'] !=0 and row['REACTIVE_POWER'] !=0:
+        return row['ACTIVE_POWER'] / math.sqrt(row['ACTIVE_POWER']**2 + row['REACTIVE_POWER']**2)
+    else:
+        return 0
+    
+zni_df['POWER_FACTOR'] =  zni_df.apply(calculate_power_factor, axis=1)
+zni_df['POWER_FACTOR']
+
+#Show first 5 zones with lowest and highest 'max power' values
+def power_factor(location,range='max'):
+    power_factor = zni_df.groupby(location).POWER_FACTOR.mean().sort_values(ascending=False)
+    power_factor = pd.DataFrame(power_factor).reset_index()
+
+    # Remove parentheses and their contents for visualization purposes
+    power_factor[location] = power_factor[location].str.replace(r'\s*\(.*\)', '', regex=True)
+
+    if range == 'max':
+        # Create bar chart
+        fig=px.bar(power_factor[:5],x=location,y="POWER_FACTOR",title="Highest 'POWER_FACTOR'")
+    else:
+        # Create bar chart
+        fig=px.bar(power_factor[-5:],x=location,y="POWER_FACTOR",title="Lowest 'POWER_FACTOR'")
+    
+    # Update marker color
+    fig.update_traces(marker=dict(color='#008080'))
+
+    # Set automargin for x-axis tick labels to True
+    fig.update_xaxes(automargin=True)
+
+    return fig
+
+
+def solar_map():
+    # Create the choropleth map
+    with rio.open('datasets\Colombia_GISdata_LTAy_YearlyMonthlyTotals_GlobalSolarAtlas-v2_GEOTIFF\PVOUT.tif') as src:  
+        solar_data = src.read(1)
+    fig= px.imshow(solar_data)
+    
+    my_layout= dict(title_text='Photovoltaic power potential [kWh/kWp] in Colombia', title_x=0.5, width =700, height=500, template='none', 
+                  coloraxis_colorbar=dict(len=0.75, thickness=25))
+
+    fig.update_traces(customdata=solar_data, hovertemplate='PVOUT [kWh/kWp]: %{customdata}<extra></extra>')
+    fig.update_layout(**my_layout)
+
+    return fig
+
+def solar_map2():
+    # Create the choropleth map
+    with rio.open('datasets\Colombia_GISdata_LTAy_YearlyMonthlyTotals_GlobalSolarAtlas-v2_GEOTIFF\GHI.tif') as src:  
+        solar_data = src.read(1)
+    fig= px.imshow(solar_data)
+    
+    my_layout= dict(title_text='Global horizontal irradiation [kWh/m2] in Colombia', title_x=0.5, width =700, height=500, template='none', 
+                  coloraxis_colorbar=dict(len=0.75, thickness=25))
+
+    fig.update_traces(customdata=solar_data, hovertemplate='GHI [kWh/m2]: %{customdata}<extra></extra>')
+    fig.update_layout(**my_layout)
+
+    return fig
+
+#def wind_map():
+    # Create the choropleth map
+    with rio.open('datasets\COL_power_density\COL_power-density_100m.tif') as src:  
+        solar_data = src.read(1)
+    fig= px.imshow(solar_data)
+    
+    my_layout= dict(title_text='Wind power in Colombia', title_x=0.5, width =700, height=500, template='none', 
+                  coloraxis_colorbar=dict(len=0.75, thickness=25))
+
+    fig.update_traces(customdata=solar_data, hovertemplate='Wind power: %{customdata}<extra></extra>')
+    fig.update_layout(**my_layout)
+
+    return fig
+
+    
